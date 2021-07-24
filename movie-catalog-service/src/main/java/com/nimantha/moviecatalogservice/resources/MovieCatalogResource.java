cd31859dbe39657fa1de7e28536709e1dd142ab1
nimanthaF <nimanthafernando95@gmail.com>
2021-07-24 19:16:19 +0530

    adding the REST template to catalog services

--- movie-catalog-service/src/main/java/com/nimantha/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/nimantha/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,13 +1,18 @@
 package com.nimantha.moviecatalogservice.resources;
 
 import com.nimantha.moviecatalogservice.models.CatalogItem;
+import com.nimantha.moviecatalogservice.models.Movie;
+import com.nimantha.moviecatalogservice.models.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
@@ -15,8 +20,21 @@ public class MovieCatalogResource {
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-        return Collections.singletonList(
-                new CatalogItem("Loki","Test",4)
+
+        RestTemplate restTemplate=new RestTemplate();
+
+
+        List<Rating> ratings= Arrays.asList(
+                new Rating("1234",4),
+                new Rating("5678",3)
         );
+
+        return ratings.stream().map(rating -> {
+            Movie movie=restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getName(), "Test", rating.getRating());
+        })
+                .collect(Collectors.toList());
+
+
     }
 }
