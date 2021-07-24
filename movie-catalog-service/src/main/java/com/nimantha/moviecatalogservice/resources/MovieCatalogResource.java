786e700773fa652d62afd97c618d3ff166fba283
nimanthaF <nimanthafernando95@gmail.com>
2021-07-24 17:29:18 +0530

    creating APIs for catalog,rating and info

---
+++ movie-catalog-service/src/main/java/com/nimantha/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,22 @@
+package com.nimantha.moviecatalogservice.resources;
+
+import com.nimantha.moviecatalogservice.models.CatalogItem;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+        return Collections.singletonList(
+                new CatalogItem("Loki","Test",4)
+        );
+    }
+}
--- movie-info-service/src/main/java/com/nimantha/movieinfoservice/MovieInfoServiceApplication.java
+++ movie-info-service/src/main/java/com/nimantha/movieinfoservice/MovieInfoServiceApplication.java
@@ -3,6 +3,7 @@ package com.nimantha.movieinfoservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class MovieInfoServiceApplication {
 
 	public static void main(String[] args) {
